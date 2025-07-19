14b8a90e666b48fef99bf3bfb6be789f3b31377a
tribalfs <tribalfs@gmail.com>
2025-07-19 23:34:19 +0800

    bottomSheet: add customizations - dodgeNavBarInset, setReleaseLowOffset, forceExpandOnNestedScrollStop and setRetainOriginalPaddings
    This commit introduces the following customization options:
    - ** A new XML attribute `dodgeNavBarInset` for `BottomSheetBehavior`. When true, the bottom sheet will adjust its height to avoid being occluded by the navigation bar.
    - **`BottomSheetBehavior.setReleaseLowOffset(int)`**: Allows setting a custom offset to determine when the dialog should be hidden upon touch release.
    - **`BottomSheetBehavior.forceExpandOnNestedScrollStop(boolean)`**:  Allows forcing the bottom sheet to always expand when a nested scrolling child stops scrolling.
    - **`BottomSheetDialog.setRetainOriginalPaddings(boolean)`**:  Allows the dialog sheet to retain its original padding when slid under the status bar, if edge-to-edge is enabled.

--- lib/java/com/google/android/material/bottomsheet/BottomSheetDialog.java
+++ lib/java/com/google/android/material/bottomsheet/BottomSheetDialog.java
@@ -87,6 +87,7 @@ public class BottomSheetDialog extends AppCompatDialog {
   private EdgeToEdgeCallback edgeToEdgeCallback;
   private boolean edgeToEdgeEnabled;
   @Nullable private MaterialBackOrchestrator backOrchestrator;
+  private boolean retainOriginalPaddings = false;//custom
 
   public BottomSheetDialog(@NonNull Context context) {
     this(context, 0);
@@ -325,6 +326,7 @@ public class BottomSheetDialog extends AppCompatDialog {
               if (insets != null) {
                 edgeToEdgeCallback = new EdgeToEdgeCallback(bottomSheet, insets);
                 edgeToEdgeCallback.setWindow(getWindow());
+                edgeToEdgeCallback.retainOriginalPaddings = retainOriginalPaddings;
                 behavior.addBottomSheetCallback(edgeToEdgeCallback);
               }
 
@@ -449,6 +451,7 @@ public class BottomSheetDialog extends AppCompatDialog {
 
     @Nullable private Window window;
     private boolean lightStatusBar;
+    private boolean retainOriginalPaddings = false;
 
     private EdgeToEdgeCallback(
         @NonNull final View bottomSheet, @NonNull WindowInsetsCompat insetsCompat) {
@@ -507,6 +510,8 @@ public class BottomSheetDialog extends AppCompatDialog {
     }
 
     private void setPaddingForPosition(View bottomSheet) {
+      if (retainOriginalPaddings) return;//custom
+
       if (bottomSheet.getTop() < insetsCompat.getSystemWindowInsetTop()) {
         // If the bottomsheet is light, we should set light status bar so the icons are visible
         // since the bottomsheet is now under the status bar.
@@ -549,5 +554,18 @@ public class BottomSheetDialog extends AppCompatDialog {
       }
       view.setSystemUiVisibility(flags);
     }
+  }
+
+  //Custom
+  /**
+   * Sets whether the dialog sheet should retain its original padding when the sheet is slid under
+   * and out the status bar.
+   *
+   * @param retain true to retain original paddings, false otherwise. This is false by default.
+   */
+  public void setRetainOriginalPaddings(boolean retain) {
+    this.retainOriginalPaddings = retain;
+    if (edgeToEdgeCallback == null) return;
+    edgeToEdgeCallback.retainOriginalPaddings = retain;
   }
 }
